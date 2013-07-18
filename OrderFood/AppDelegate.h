fca4742d2f2c075556ed59e05a88cd5328a493f4
aplle <aplle@192.168.1.3>
2013-07-18 21:02:09 +0800

    Initial Commit

---
+++ OrderFood/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OrderFood
+//
+//  Created by aplle on 7/18/13.
+//  Copyright (c) 2013 handcent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
